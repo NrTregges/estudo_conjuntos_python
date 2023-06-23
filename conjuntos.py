383e5f604c47c874870791bec4493fe8e9ac08a7
NrTregges <136847458+NrTregges@users.noreply.github.com>
2023-06-23 15:54:02 -0300

    Add files via upload

---
+++ conjuntos.py
@@ -0,0 +1,34 @@
+"""
+Programa Conjuntos
+Descrição: Programa que apresenta a estrutura conjuntos em Python
+para fins de aprendizado.
+Autor: Nicholas R. Tregges.
+Data: 23/06/2023
+Versão: 0.0.1
+"""
+
+# Atribuição de Variáveis
+
+lista = [1,2,3,3,5,7,9,11,31,8,8,47]
+conjunto_2 = {1,3,3,5,5,0,0,23,-2}
+
+
+
+# Entrada de dados
+
+conjunto_1 = set(lista)
+
+
+
+
+# Processamento de dados
+
+conjunto_2.add(49)
+conjunto_2.remove(5)
+
+
+
+# Saída de dados
+
+print(conjunto_1)
+print(conjunto_2)
